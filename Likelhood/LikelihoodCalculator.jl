1c96efe200a298caa15d46a29dfdf5ddc5180fdd
erathorn <wahle.joh@gmail.com>
2019-06-11 15:50:17 +0200

    SIMD and debug

--- Likelhood/LikelihoodCalculator.jl
+++ Likelhood/LikelihoodCalculator.jl
@@ -14,29 +14,41 @@ This function calculates the log-likelihood of an evolutiuonary model using the
 Felsensteins pruning algorithm.
 """
 function FelsensteinFunction(tree_postorder::Vector{Node}, pi::Number)#, rates::Vector{Float64})
+    root = last(tree_postorder)
+    n_c::Int64 = size(root.data)[2]
     for node in tree_postorder
         if node.nchild != 0
             left_daughter = node.child[1]
             right_daughter = node.child[2]
-            for ind in 1:size(left_daughter.data)[2]
+            linc::Float64 = left_daughter.inc_length
+            rinc::Float64 = right_daughter.inc_length
+            # use the inbounds decorator to enable SIMD
+            # SIMD greatly improves speed!!!
+            @inbounds for ind in 1:n_c
                 #r = rates[ind]
-                r = 1.0
-                left_mat = exponentiate_binary(pi, left_daughter.inc_length, r)
-                right_mat = exponentiate_binary(pi, right_daughter.inc_length, r)
+                r = 1.0 # r is going to vary
+                left_mat = exponentiate_binary(pi, linc, r)
+                right_mat = exponentiate_binary(pi, rinc, r)
+                """
+                For legacy and debugging purposes:
+                #a = left_daughter.data[1,ind]*left_mat[1,1] + left_daughter.data[2,ind]*left_mat[2,1]
+                #b = left_daughter.data[2,ind]*left_mat[2,2] + left_daughter.data[1,ind]*left_mat[1,2]
+                #c = right_daughter.data[1,ind]*right_mat[1,1] + right_daughter.data[2,ind]*right_mat[2,1]
+                #d = right_daughter.data[2,ind]*right_mat[2,2] + right_daughter.data[1,ind]*right_mat[1,2]
+                #node.data[1,ind] = a*c
+                #node.data[2,ind] = b*d
+                """
+                a = sum(left_mat.*left_daughter.data[:,ind], dims=1)
+                c = sum(right_mat.*right_daughter.data[:,ind], dims=1)
+                node.data[:,ind] = a.*c
 
-                a = (left_daughter.data[1,ind]*left_mat[1,1]) + (left_daughter.data[2,ind]*left_mat[1,2])
-                b = left_daughter.data[2,ind]*left_mat[2,2] + left_daughter.data[1,ind]*left_mat[2,1]
-                c = right_daughter.data[1,ind]*right_mat[1,1] + right_daughter.data[2,ind]*right_mat[1,2]
-                d = right_daughter.data[2,ind]*right_mat[2,2] + right_daughter.data[1,ind]*right_mat[2,1]
-                node.data[1,ind] = a*c
-                node.data[2,ind] = b*d
             end # for
         end # if
 
     end # for
-    root = last(tree_postorder)
+
     # sum the two rows
-    return log(sum(exp.(sum(log.(root.data.*[pi, 1.0-pi])))))
+    return sum(log.(root.data.*[pi, 1.0-pi]))
 end # function
 
 end  # module LikelihoodCalculator
