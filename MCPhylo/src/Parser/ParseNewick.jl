ff3f85e10588b8ebb1693e9e011cfa83918f421d
ansable <anchous12@gmail.com>
2020-04-04 17:04:25 +0200

    test newick file, an idea of the program

---
+++ MCPhylo/src/Parser/ParseNewick.jl
@@ -0,0 +1,30 @@
+
+
+"""
+    ParseNewick(filename::String)
+
+This function parses a Newick file
+"""
+function ParseNewick()
+    # for simplicity's sake I split everything in tiny methods
+end
+
+
+# TODO: function to load Newick file and get the string
+# Question: should we go with filename ot file-path? just some julia thing?
+function load_newick(filename::String)
+    open(filename, "r") do file
+        global content = readlines(file)
+    end
+    content, content[1]
+end
+
+# TODO:check if the brackets match etc
+function is_valid_newick_string
+end
+
+# TODO: create a dataframe, similar to the nexus file
+function createNewickdf
+end
+
+println(load_newick((pwd()*"\\newick_test_one.txt")))
