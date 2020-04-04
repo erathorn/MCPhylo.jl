cc9e2590e24b1080175e4be725439612db59b529
ansable <anchous12@gmail.com>
2020-04-04 19:22:34 +0200

    parse newick updated; dummy examples for newick/nexus added
    written via teletype with Adham
    * parse_newick: + valid_function, load_function (imperfect: line breaks not checked)
    * dummy files: + four example newick files, + two example nexus files (temporal location, to be used as inspiration for testing)

--- MCPhylo/src/Parser/ParseNewick.jl
+++ MCPhylo/src/Parser/ParseNewick.jl
@@ -5,8 +5,14 @@
 
 This function parses a Newick file
 """
-function ParseNewick()
+function ParseNewick(filename::String)
     # for simplicity's sake I split everything in tiny methods
+    content = load_newick(filename)
+    if !is_valid_newick_string(content)
+        throw("$filename is not a Newick file!")
+    #the parse thing
+
+
 end
 
 
@@ -20,11 +26,38 @@ function load_newick(filename::String)
 end
 
 # TODO:check if the brackets match etc
-function is_valid_newick_string
+function is_valid_newick_string(newick::String)
+    # Step one: does the stripped string ends with ';'
+    if endswith(strip(newick),";")
+        bracket_level = 0
+        for char in newick
+            if char == '('
+                bracket_level += 1
+            elseif char == ')'
+                    bracket_level -= 1
+            end #elseif
+        end #for
+        if bracket_level != 0
+            return false
+        end #if
+
+    #end # if (endswith one)
+    else
+        return false
+    end # else
+    return true
 end
 
 # TODO: create a dataframe, similar to the nexus file
 function createNewickdf
 end
 
-println(load_newick((pwd()*"\\newick_test_one.txt")))
+test_bad_brackets = load_newick((pwd()*"\\newick_test_brackets_fucked.txt"))
+test_bad_ending = load_newick((pwd()*"\\newick_test_no_column.txt"))
+test_all_gut = load_newick((pwd()*"\\newick.nwk"))
+test_new_lines = load_newick((pwd()*"\\newick_new_lines.txt"))
+println(test_new_lines)
+println(is_valid_newick_string(test_new_lines))
+# println(is_valid_newick_string(test_bad_brackets))
+# println(is_valid_newick_string(test_bad_ending))
+# println(is_valid_newick_string(test_all_gut))
