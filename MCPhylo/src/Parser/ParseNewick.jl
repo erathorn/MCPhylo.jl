8237ac7f97eebcb3e364151e56d85dd0d3d84e2f
ansable <anchous12@gmail.com>
2020-04-07 01:53:06 +0200

    update of the work (for consistency's sake)
    worked with Adham via teletype
    twisted in the wrong direction (our understanding was a bit skewed), so most of the functions will be rewritten or deleted

--- MCPhylo/src/Parser/ParseNewick.jl
+++ MCPhylo/src/Parser/ParseNewick.jl
@@ -1,4 +1,6 @@
 
+# TODO: organize imports in some reasonable way
+#include("../Tree/Node_Type.jl")
 
 """
     ParseNewick(filename::String)
@@ -10,8 +12,11 @@ function ParseNewick(filename::String)
     if !is_valid_newick_string(content)
         throw("$filename is not a Newick file!")
     end # if
+    content = strip(content)
+
     #the parse thing
     # TODO: actually useful part of the code goes here
+
 
 end
 
@@ -57,16 +62,67 @@ function is_valid_newick_string(newick::String)
     return true
 end
 
+"""
+    parse_name_length(newick::String)
+
+This function parses two optional elements of the tree, name and length. In case, when neither of this is provided, empty string and nothing are return
+"""
 
 function parse_name_length(newick::String)
     if occursin(':',newick)
         name, length = split(newick,':')
         return name, length
     end # if
-    newick, nothing
+    "no_name", nothing
 end
 
-print(parse_name_length("A:1"))
+# TODO: rewrite this one
+#
+# function make_node(newick::String)
+#     parts = split(newick, ')')
+#     if length(parts) == 1
+#         label = newick
+#         children = Vector{Node}(undef, 0)
+#         name, inc_length = parse_name_length(label)
+#         return Node{Float64,Array{Float64,2},Array{Float64},Int64}(name,ones(3,3),missing,children,ones(3),3,false,inc_length,"0",1,0.5,nothing,nothing,true)
+#     else
+#         # TODO: why can't we use length? check
+#         len_minus_one = size(parts,1)-1
+#         children = list(parse_siblings(join(parts[len_minus_one],')')[2:size(parts,1)]))
+#         label = parts[len_minus_one]
+#     end #if
+#     name, inc_length = parse_name_length(label)
+#     parent = Node(name,ones(3,3),missing,children,length(children),false,inc_length,"0",1,0.5,nothing,nothing,true)
+#     for x in children
+#         x.mother=parent
+#     end #for
+#     return parent
+# end #function
+#
+# tmp = Node()
+# println("our thing")
+# println(make_node(";"))
+
+
+function parse_siblings(newick::String)
+    bracket_lvl = 0
+    current = []
+    for c in  (newick * ',')
+        if c == ','
+            if bracket_lvl == 0
+                yield(make_node(join(current,"")))
+                current = []
+            else
+                if c == '('
+                    bracket_lvl += 1
+                elseif c == ')'
+                    bracket_lvl -= 1
+                end #elseif
+            end #if
+            push!(current,c)
+        end #if
+    end #for
+end #function
 
 # TODO: create a dataframe, similar to the nexus file
 function createNewickdf
