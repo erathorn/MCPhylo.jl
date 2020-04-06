56490eb3a8672cc472535afba04e411aec4ebce9
Arcumenn <steinar.graessel@gmx.net>
2020-04-06 18:33:22 +0200

    add iteration to algorithm

--- MCPhylo/src/Tree/Neighbor_joining.jl
+++ MCPhylo/src/Tree/Neighbor_joining.jl
@@ -1,58 +1,81 @@
+"""
+neighbor_joining(dm::Array{Int64,2}, tree::T)
+
+This function returns a phylogenetic tree by using neighbor joining
+on the given tree.
+"""
+# Tree structure, node naming, node numbers needed?, example tree? 
 function neighbor_joining(dm::Array{Int64,2}, tree::T) where {T<:AbstractNode}
-    matrix_size = size(dm)
-    n = matrix_size[1]
+
+    n = size(dm)[1]
     leaves = get_leaves(tree)
-    # df = DataFrame()
-    # leaf_names = [name for leaves.name in leaves]
-    # for i in 1:n
-    #     df.leaves[i].name = dm[:,i]
-    q1 = zeros(Float64, n, n)
-    for i = 1:n
-        for j = i+1:n
-            q1[i, j] =
+
+    while n > 2
+        q1 = zeros(Float64, n, n)
+        for i = 1:n
+            for j = i+1:n
+                q1[i, j] =
                 q1[j, i] = (n - 2) * dm[i, j] - sum(dm[i, :]) - sum(dm[j, :])
+            end # end for
         end # end for
-    end # end for
-    index = findmin(q1)[2]
-    first_node = leaves[index[1]]
-    second_node = leaves[index[2]]
-    blub = Node("Blub")
-    blub.children = [first_node, second_node]
-    blub.nchild = 2
-    first_node.mother = blub
-    second_node.mother = blub
-    first_distance =
-        0.5 * dm[index] + (1 / (2 * (n - 2))) * (sum(dm[i, :]) - sum(dm[j, :]))
-    first_node.inc_length = first_distance
-    second_node.inc_length = dm[index] - first_distance
+        count = 1
+        index = findmin(q1)[2]
+        first_node = leaves[index[1]]
+        second_node = leaves[index[2]]
+        new_node = Node("Node_$count")
+        new_node.children = [first_node, second_node]
+        new_node.nchild = 2
+        first_node.mother = new_node
+        second_node.mother = new_node
+        deleteat!(leaves, [index[1], index[2]])
+        insert!(leaves, 1, new_node)
 
-    dm2 = zeros(Float64, n-1, n-1)
-    let j = 1
-        for i = 2:n-1
-            while  j == index[1] || j == index[2]
+        first_node.inc_length =
+        0.5 * dm[index] + (1 / (2 * (n - 2))) * (sum(dm[index[1], :]) - sum(dm[index[2], :]))
+        second_node.inc_length = dm[index] - first_node.inc_length
+
+        next_dm = zeros(Float64, n-1, n-1)
+        let j = 1
+            for i = 2:n-1
+                while  j == index[1] || j == index[2]
+                    j += 1
+                end # end while
+                next_dm[i,1] = next_dm[1,i] =  0.5 * (dm[index[1],j] + dm[index[2],j] - dm[index[1], index[2]])
                 j += 1
-            end # end while
-            dm2[i,1] = dm2[1,i] =  0.5 * (dm[index[1],j] + dm[index[2],j] - dm[index[1],index[2]])
-            j += 1
-        end
-    end
-    for k = 2:n-1
-        for l = 2:n-1
-            for i = 1:n
-                if i == index[1] || i == index[2]
-                    continue
-                end
-                for j = 1:n
-                    if j == index[1] || j == index[2]
-                        print("I'm here")
-                    else
-                        dm2[k,l] = dm[i,j]
-                        print("update")
-                    end
-                end
-            end
-        end
-    return dm2
-    end
+            end # end for
+        end # end let
+        entries = zeros(0)
 
+        for i = 1:n
+            if i == index[1] || i == index[2]
+                continue
+            end # end if
+            for j = 1:n
+                if j == index[1] || j == index[2]
+                    continue
+                else
+                    append!(entries, dm[i,j])
+                end # end else
+            end # end for
+        end # end for
+
+        let z = 1
+            for k = 2:n-1
+                for l = 2:n-1
+                    next_dm[k,l] = entries[z]
+                    z += 1
+                end   # end for
+            end # end for
+        end # end let
+        n -= 1 # update matrix dimension
+        dm = next_dm
+        println(dm)
+        count += 1
+    end # end while
+    root = last(leaves)
+    root.children = [leaves[1]]
+    root.nchild = 1
+    leaves[1].mother = root
+
+    return root
 end # end function neighbor_joining
