83d7b6d6d32487676388686826f339dcf9643232
Steinar Graessel <steinar.graessel@student.uni-tuebingen.de>
2020-04-16 14:38:18 +0200

    add some print statements and functions for testing

--- test_neighborjoining.jl
+++ test_neighborjoining.jl
@@ -15,6 +15,13 @@ a newick string and save it into a file as follows
 """
 tree = neighbor_joining(distance_matrix, leaves_list)
 f = open("newick_output.nwk", "w")
+
+node_height(tree)
+println(tree)
+println(typeof(tree))
+println(tree.name)
+println(tree.height)
+
 println(f, newick(tree))
 close(f)
 
@@ -26,3 +33,29 @@ to the gold standard one in newick1.nwk (respectively for newick2, and newick3)
 Apparently they can also compute a neighbor joining tree from a distance matrix,
 if you find it necessary, you can also use these methods for comparison.
 """
+
+function newick(root::T)::String  where T<:Node
+    # get the newickstring
+    newickstring = newick(root, "")
+
+    # Some polishing.
+    newickstring = chop(newickstring)
+    newickstring = string(newickstring, ";")
+    return newickstring
+end
+
+function node_height(root::T)  where T<:Node
+    for node in post_order(root)
+        if node.nchild == 0
+            node.height = 0
+        else
+            node.height = maximum([child.inc_length+child.height for child in node.children])
+        end
+    end
+end # function node_height
+
+function post_order(root::T)::Vector{T} where T<:Node
+    t::Vector{T} = []
+    post_order(root, t)
+    return t
+end # function post_order
