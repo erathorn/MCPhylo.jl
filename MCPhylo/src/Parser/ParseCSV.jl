25c50295590e74569298dda9455d6cc9a47aec8e
ansable <anchous12@gmail.com>
2020-05-15 19:00:28 +0200

    Update ParseCSV.jl

--- MCPhylo/src/Parser/ParseCSV.jl
+++ MCPhylo/src/Parser/ParseCSV.jl
@@ -1,17 +1,15 @@
 """
-    ParseNexus(filename::String)
+    ParseCSV(filename::String)
 
-This function parses a NEXUS file which stores the input for the MCMC compuation.
+This function parses a CSV file which stores the input for the MCMC compuation.
 The file should follow the conventions used for MrBayes.
 """
-function ParseCSV(filename::String, header::Bool=True)
+function ParseCSV(filename::String, header::Bool=true)
     open(filename, "r") do file
         global content = readlines(file)
     end # do
-
     # remove the header if necessary
     header ||  popfirst!(content)
-
     df = create_csvdf(content)
     dimensions = size(df)
     ntax = dimenseions[1]
@@ -28,10 +26,12 @@ This function parses a CSV file and returns its content as a DataFrame.
 NOTE: May later on be replaced by the respective DataFrames function.
 """
 function create_csvdf(filecontent::Array{String}, separator::AbstractString=",")::DataFrame
-    df = DataFrame(Language=String[], Data=String[])
+    df = DataFrame(Language = String, Data = String[])
+
     while !isempty(filecontent)
         line = popfirst!(filecontent)
-        push!(df, split(line))
+        line = rsplit(line,'_')
+        push!(df, Dict(:Language => string(line[1]), :Data => line[2]))
     end # while
     return df
 end # function ParseCSV
