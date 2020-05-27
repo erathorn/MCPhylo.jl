a7d6104f2452f3dc4cb9dccb1d65ed19422ad58f
Arcumenn <steinar.graessel@gmx.net>
2020-05-27 14:01:20 +0200

    remove Gadfly import; add Plots import

--- MCPhylo/src/MCPhylo.jl
+++ MCPhylo/src/MCPhylo.jl
@@ -15,6 +15,10 @@ using CuArrays
 using GPUArrays
 using CUDAnative
 using CUDAdrv
+import Plots
+import StatsPlots
+using Plots
+using StatsPlots
 
 using Zygote
 using Calculus
@@ -57,8 +61,6 @@ import Distributions:
        ## Methods
        cdf, dim, gradlogpdf, insupport, isprobvec, logpdf, logpdf!, maximum,
        minimum, pdf, quantile, rand, sample!, support, length
-import Gadfly: draw, Geom, Guide, Layer, layer, PDF, PGF, Plot, plot, PNG, PS,
-       render, Scale, SVG, Theme
 using LightGraphs: DiGraph, add_edge!, outneighbors,
        topological_sort_by_dfs, vertices
 import StatsBase: autocor, autocov, countmap, counts, describe, predict,
