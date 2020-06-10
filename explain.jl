120f338ba424ac5818ed22679704ef18942df643
Arcumenn <steinar.graessel@gmx.net>
2020-06-10 13:50:11 +0200

    update to check contourplot

--- explain.jl
+++ explain.jl
@@ -31,53 +31,51 @@ samplers = [NUTS(:μ),
 
 setsamplers!(model, samplers)
 
-sim = mcmc(model, my_data, inits, 10000, burnin=500,thin=5, chains=2)
+sim = mcmc(model, my_data, inits, 5000, burnin=500,thin=5, chains=2)
 
-plots = MCPhylo.plotMC(sim)
+theme(:solarized)
+plots = MCPhylo.plotMC(sim, legend=true)
+plots = MCPhylo.plotMC(sim, :mixeddensity)
 MCPhylo.draw(plots)
+
 
 write("sim.jls", sim)
 mysim = read("sim.jls", ModelChains)
 
-
-
-
-
 using StatsBase
-function mybarplot(c::AbstractChains; legend::Bool=false,
-                  position::Symbol=:stack, na...)
+function mycontourplot(c::AbstractChains; bins::Integer=100, na...)
   nrows, nvars, nchains = size(c.value)
-  plots = Array{Plots.Plot}(undef, nvars)
-  pos = legend ? :right : :none
-  for i in 1:nvars
-    S = unique(c.value[:, i, :])
-    n = length(S)
-    x = repeat(S, 1, nchains)
-    y = zeros(n, nchains)
-    for j in 1:nchains
-      m = countmap(c.value[:, i, j])
+  # new list initialization
+  plots = Plots.Plot[]
+  offset = 1e4 * eps()
+  n = nrows * nchains
+  for i in 1:(nvars - 1)
+    X = c.value[:, i, :]
+    qx = range(minimum(X) - offset, stop=maximum(X) + offset, length=bins + 1)
+    mx = map(k -> mean([qx[k], qx[k + 1]]), 1:bins)
+    idx = Int[findfirst(k -> qx[k] <= x < qx[k + 1], 1:bins) for x in X]
+    for j in (i + 1):nvars
+      Y = c.value[:, j, :]
+      qy = range(minimum(Y) - offset, stop=maximum(Y) + offset, length=bins + 1)
+      my = map(k -> mean([qy[k], qy[k + 1]]), 1:bins)
+      idy = Int[findfirst(k -> qy[k] <= y < qy[k + 1], 1:bins) for y in Y]
+      density = zeros(bins, bins)
       for k in 1:n
-        if S[k] in keys(m)
-          y[k, j] = m[S[k]] / nrows
-        end
+        density[idx[k], idy[k]] += 1.0 / n
       end
+
+      # new plot creation block, based on Plots with a GR backend
+      p = Plots.plot(mx, my, density, seriestype=:contour,
+                     colorbar_title="Density", xlabel=c.names[i],
+                     ylabel=c.names[j])
+      push!(plots, p)
     end
-    ymax = maximum(position == :stack ? mapslices(sum, y, dims=2) : y)
-    # new plot creation block, based on StatsPlots with a GR backend
-    datapoints = hcat(vec(x), vec(y))
-    plots[i] = StatsPlots.groupedbar(datapoints, bar_position = position,
-                                     linecolor=:match,
-                                     group=repeat(c.chains, inner=[n]),
-                                     legendtitle="Chain", xlabel = "Value",
-                                     ylabel = "Density", title=c.names[i],
-                                     legend = pos, ylims=(0.0, ymax),
-                                     grid=:dash, gridalpha=0.5)
   end
   return plots
 end
-theme(:solarized_light)
 
-myplots = mybarplot(mysim)
+myplots = mycontourplot(mysim)
+MCPhylo.draw(myplots)
 myplots[1]
 myplots[2]
 myplots[3]
