5f3a79eb225253717cebb890981066ea0be04bca
Arcumenn <steinar.graessel@gmx.net>
2020-08-06 15:44:11 +0200

    add missing 'begin' statements; bigger tree examples to test recursion

--- doc/Tree/ladderize.jl
+++ doc/Tree/ladderize.jl
@@ -2,11 +2,12 @@ include("../../src/MCPhylo.jl")
 using .MCPhylo
 using Test
 
-start_tree = MCPhylo.parsing_newick_string("((A,(B,(C,(D,E)))),(F,(G,H)))")
-descending_tree = MCPhylo.parsing_newick_string("(((((D,E),C),B),A),((G,H),F))")
-ascending_tree = MCPhylo.parsing_newick_string("((F,(G,H)),(A,(B,(C,(D,E)))))")
+@testset "Tree Ladderizing" begin
 
-@testset "Tree Ladderizing"
+
+    start_tree = MCPhylo.parsing_newick_string("((A,(B,(C,(D,E)))),(F,(G,H)))")
+    descending_tree = MCPhylo.parsing_newick_string("(((((D,E),C),B),A),((G,H),F))")
+    ascending_tree = MCPhylo.parsing_newick_string("((F,(G,H)),(A,(B,(C,(D,E)))))")
 
     start_newick = newick(start_tree)
     desc_newick = newick(descending_tree)
@@ -15,7 +16,7 @@ ascending_tree = MCPhylo.parsing_newick_string("((F,(G,H)),(A,(B,(C,(D,E)))))")
     ladderized_asc_tree = MCPhylo.ladderize_tree(start_tree)
     @test newick(ladderized_asc_tree) == asc_newick
 
-    ladderized_desc_tree =  MCPhylo.ladderize_tree(start_tree,false)
+    ladderized_desc_tree = MCPhylo.ladderize_tree(start_tree,false)
     @test newick(ladderized_desc_tree) == desc_newick
 
     MCPhylo.ladderize_tree!(start_tree)
