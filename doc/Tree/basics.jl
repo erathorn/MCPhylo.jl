ad0a7401d18e3f22836663e6cc3a13df11926b82
Arcumenn <steinar.graessel@gmx.net>
2020-08-06 15:44:57 +0200

    tests for remove_node and insert_node

---
+++ doc/Tree/basics.jl
@@ -0,0 +1,42 @@
+include("../../src/MCPhylo.jl")
+using .MCPhylo
+using Test
+
+@testset "insert_node" begin
+
+    tree = MCPhylo.parsing_newick_string("(A,B,(C,D)E)F;")
+    insert_tree = MCPhylo.parsing_newick_string("(A,B,((C,D)no_name)E)F;")
+    insert_tree_newick = newick(insert_tree)
+
+    MCPhylo.number_nodes!(tree)
+
+    children = [find_by_name(tree, "C"), find_by_name(tree, "D")]
+    mother = find_by_name(tree, "E")
+
+    insert_node!(mother, children)
+    MCPhylo.number_nodes!(tree)
+
+    @test newick(tree) == insert_tree_newick
+
+    new_node = Node("G")
+    append!(children, new_node)
+    @test_throws AssertionError insert_node!(mother, children)
+
+ end
+
+@testset "remove_node" begin
+
+    tree = MCPhylo.parsing_newick_string("(A,B,(C,D)E)F;")
+    remove_tree = MCPhylo.parsing_newick_string("(A,B,C,D)F;")
+    remove_tree_newick = newick(remove_tree)
+
+    MCPhylo.number_nodes!(tree)
+
+    node_e = find_by_name(tree, "E")
+    remove_node!(node_e)
+    @test newick(tree) == remove_tree_newick
+
+    node_f = find_by_name(tree, "F")
+    @test_throws ArgumentError remove_node!(node_f)
+
+end
