d3e4491f11d9adcdf513030aec8832c2d3c74b0f
Arcumenn <steinar.graessel@gmx.net>
2020-08-19 15:30:15 +0200

    finish 'find_common_cluster' function (based on Day's algorithm)

--- src/Tree/Tree_Consensus.jl
+++ src/Tree/Tree_Consensus.jl
@@ -1,11 +1,62 @@
-function days_algorithm(cluster1::T, cluster2::T) where T<:AbstractNode
+"""
+    find_common_clusters(tree:T)
+        ::Tuple{Dict{String, Int64}, Dict{Tuple{Int64, Int64}, Int64}} where T<:AbstractNode
 
+Use Day's algorithm to find all clusters of tree2 that also occur in tree1
+"""
+function find_common_clusters(ref_tree::T, tree::T)::Tuple{Vector{Vector{Node}}, Vector{Vector{Node}}} where T<:AbstractNode
+    ref_leaf_names = [leaf.name for leaf in get_leaves(ref_tree)]
+    leaf_names = [leaf.name for leaf in get_leaves(tree)]
+    if length(ref_leaf_names) != length(leaf_names)
+        throw(ArgumentError("The leaf label sets of the trees are not identical"))
+    elseif length(ref_leaf_names) != length(Set(append!(ref_leaf_names, leaf_names)))
+        throw(ArgumentError("The leaf label sets of the trees are not identical"))
+    end
+    nodes = post_order(ref_tree)
+    leaves_dict = Dict{String, Int64}()
+    cluster_dict = Dict{Tuple{Int64, Int64}, Int64}()
+    dict_count = 1
+    for node in nodes
+        if node.nchild == 0
+            leaves_dict[node.name] = dict_count
+            dict_count += 1
+        else
+            leaves = get_leaves(node)
+            if node.root == true || node == node.mother.children[1]
+                last_index = leaves_dict[last(leaves).name]
+                first_index = leaves_dict[first(leaves).name]
+                cluster_dict[(first_index, last_index)] = last_index
+            else
+                last_index = leaves_dict[last(leaves).name]
+                first_index = leaves_dict[first(leaves).name]
+                cluster_dict[(first_index, last_index)] = first_index
+            end # if
+        end # if
+    end # for
+    occuring_clusters, missing_clusters = Vector{Vector{Node}}(), Vector{Vector{Node}}()
+    nodes = post_order(tree)
+    for node in nodes
+        if node.nchild != 0
+            leaves = get_leaves(node)
+            cluster_indeces = [leaves_dict[node.name] for node in leaves]
+            if length(get_leaves(node)) != maximum(cluster_indeces) -
+                                           minimum(cluster_indeces) + 1
+                push!(missing_clusters, leaves)
+            elseif haskey(cluster_dict, (minimum(cluster_indeces), maximum(cluster_indeces)))
+                push!(occuring_clusters, leaves)
+            else
+                push!(missing_clusters, leaves)
+            end # if
+        end # if
+    end # for
+    return occuring_clusters, missing_clusters
 end
+
 
 """
     are_compatible(cluster_names::Vector{String}, tree::T) where T<:AbstractNode
 
-Check if a cluster of nodes, derived from the node names is compatible with a tree
+Check if a cluster of nodes (identified by their names) is compatible with a tree
 """
 function are_compatible(cluster::Vector{String}, tree::T)::Bool where T<:AbstractNode
     leaf_names = [leaf.name for leaf in get_leaves(tree)]
@@ -19,13 +70,13 @@ function are_compatible(cluster::Vector{String}, tree::T)::Bool where T<:Abstrac
         if !(length(intersect(leaves, cluster)) in (0, length(leaves)))
             return false
         end #if
-    end # end
+    end # for
     return true
 end
 
 
 """
-    are_compatible(cluster::Vector{T}, tree::T) where T<:AbstractNode
+    are_compatible(cluster::Vector{T}, tree::T)::Bool where T<:AbstractNode
 
 Check if a cluster of nodes is compatible with a tree
 """
@@ -36,7 +87,7 @@ end
 
 
 """
-    majority_consensus_tree(trees::Vector{T}, tree::T)::T where T<:AbstractNode
+    majority_consensus_tree(trees::Vector{T})::T where T<:AbstractNode
 
 Construct the majority rule consensus tree from a set of trees
 """
