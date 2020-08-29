cf480e4e6e03b7f7d4eb66169a68166f315cab72
Arcumenn <steinar.graessel@gmx.net>
2020-08-29 19:46:33 +0200

    update find_com_clusters test; add del_mark_nodes / maj_cons_tree tests

--- doc/Tree/consensus.jl
+++ doc/Tree/consensus.jl
@@ -10,48 +10,38 @@ using Test
     tree2 = MCPhylo.parsing_newick_string("((G,(C,(A,(F,E)))),(B,(D,H)))")
     MCPhylo.number_nodes!(tree2)
     MCPhylo.set_binary!(tree2)
-
     A, B, C, D, E, F, G, H = find_by_name(tree2, "A"), find_by_name(tree2, "B"),
                              find_by_name(tree2, "C"), find_by_name(tree2, "D"),
                              find_by_name(tree2, "E"), find_by_name(tree2, "F"),
                              find_by_name(tree2, "G"), find_by_name(tree2, "H")
-
-    expected_matches = [G.mother.mother]
-    expected_mis_matches = [F.mother, A.mother, C.mother, G.mother, D.mother, B.mother]
-    matches, mis_matches = MCPhylo.find_common_clusters(ref_tree, tree2)
-    @test matches == expected_matches
-    @test mis_matches == expected_mis_matches
+     expected_dict = Dict([(D.mother, false), (C.mother, false), (B.mother, false),
+                           (A.mother, false), (G.mother, false), (F.mother, false),
+                           (G.mother.mother, true)])
+    @test MCPhylo.find_common_clusters(ref_tree, tree2) == expected_dict
 
     tree3 = MCPhylo.parsing_newick_string("((A,(C,(D,(B,E)))),(G,(F,H)))")
     MCPhylo.number_nodes!(tree3)
     MCPhylo.set_binary!(tree3)
-
     A, B, C, D, E, F, G, H = find_by_name(tree3, "A"), find_by_name(tree3, "B"),
                              find_by_name(tree3, "C"), find_by_name(tree3, "D"),
                              find_by_name(tree3, "E"), find_by_name(tree3, "F"),
                              find_by_name(tree3, "G"), find_by_name(tree3, "H")
-
-    expected_matches = [C.mother, A.mother, G.mother, A.mother.mother]
-    expected_mis_matches = [B.mother, D.mother, F.mother]
-    matches, mis_matches = MCPhylo.find_common_clusters(ref_tree, tree3)
-    @test matches == expected_matches
-    @test mis_matches == expected_mis_matches
+     expected_dict = Dict([(D.mother, false), (C.mother, true), (B.mother, false),
+                           (A.mother, true), (G.mother, true), (F.mother, false),
+                           (A.mother.mother, true)])
+    @test MCPhylo.find_common_clusters(ref_tree, tree3) == expected_dict
 
     tree4 = MCPhylo.parsing_newick_string("((A,(B,(C,(D,E)))),(F,(G,H)))")
     MCPhylo.number_nodes!(tree4)
     MCPhylo.set_binary!(tree4)
-
     A, B, C, D, E, F, G, H = find_by_name(tree4, "A"), find_by_name(tree4, "B"),
                              find_by_name(tree4, "C"), find_by_name(tree4, "D"),
                              find_by_name(tree4, "E"), find_by_name(tree4, "F"),
                              find_by_name(tree4, "G"), find_by_name(tree4, "H")
-
-    expected_matches = [D.mother, C.mother, B.mother, A.mother, G.mother, F.mother,
-                        A.mother.mother]
-    expected_mis_matches = Vector{Node}()
-    matches, mis_matches = MCPhylo.find_common_clusters(ref_tree, tree4)
-    @test matches == expected_matches
-    @test mis_matches == expected_mis_matches
+    expected_dict = Dict([(D.mother, true), (C.mother, true), (B.mother, true),
+                          (A.mother, true), (G.mother, true), (F.mother, true),
+                          (A.mother.mother, true)])
+    @test MCPhylo.find_common_clusters(ref_tree, tree4) == expected_dict
 
     tree5 = MCPhylo.parsing_newick_string("((G,(X,(A,(F,E)))),(B,(D,H)))")
     MCPhylo.number_nodes!(tree5)
@@ -64,10 +54,29 @@ using Test
     @test_throws ArgumentError MCPhylo.find_common_clusters(ref_tree, tree6)
 end
 
+@testset "delete_marked_nodes!" begin
+    tree = MCPhylo.parsing_newick_string("(A,B,(C,(D,E)F)G)H;")
+    MCPhylo.set_binary!(tree)
+    MCPhylo.number_nodes!(tree)
+    marked = Dict([(1, false), (2, false), (3, false), (4, false),
+                   (5, false),  (6, true), (7, true), (8, false)])
+    MCPhylo.delete_marked_nodes!(tree, marked)
+    @test newick(tree) == newick(MCPhylo.parsing_newick_string("(A,B,C,D,E)H;"))
+
+    tree = MCPhylo.parsing_newick_string("(A,B,(C,(D,E)F)G)H;")
+    MCPhylo.set_binary!(tree)
+    MCPhylo.number_nodes!(tree)
+    marked = Dict([(1, false), (2, false), (3, false), (4, false),
+                   (5, false),  (6, false), (7, true), (8, false)])
+    MCPhylo.delete_marked_nodes!(tree, marked)
+    @test newick(tree) == newick(MCPhylo.parsing_newick_string("(A,B,C,(D,E)F)H;"))
+end
+
 @testset "order_tree!" begin
     tree = MCPhylo.parsing_newick_string("(A,B,(C,(D,E)F)G)H;")
     MCPhylo.set_binary!(tree)
     MCPhylo.number_nodes!(tree)
+
     A, B, C, D, E, F, G, H = find_by_name(tree, "A"), find_by_name(tree, "B"),
                              find_by_name(tree, "C"), find_by_name(tree, "D"),
                              find_by_name(tree, "E"), find_by_name(tree, "F"),
@@ -81,8 +90,8 @@ end
     MCPhylo.set_binary!(ordered_tree)
 
     @test MCPhylo.order_tree!(tree, cluster_start_indeces) == [A, E, D, C, B]
-    MCPhylo.set_binary!(tree)
     MCPhylo.number_nodes!(tree)
+    MCPhylo.set_binary!(tree)
     @test tree == ordered_tree
 end
 
@@ -199,4 +208,15 @@ end
 
     trees = [tree1, tree2, tree3, tree4]
     @test_throws ArgumentError MCPhylo.majority_consensus_tree(trees)
+end
+
+@testset "majority_consensus_tree" begin
+    tree1 = MCPhylo.parsing_newick_string("(((A,B),C),(D,E))")
+    tree2 = MCPhylo.parsing_newick_string("((A,C),(B,D,E))")
+    tree3 = MCPhylo.parsing_newick_string("(((B,C),A),D,E)")
+    trees = [tree1, tree2, tree3]
+    MCPhylo.number_nodes!.(trees)
+    MCPhylo.set_binary!.(trees)
+    result = newick(MCPhylo.parsing_newick_string("((A,B,C),D,E)"))
+    @test MCPhylo.majority_consensus_tree(trees) == result
 end
