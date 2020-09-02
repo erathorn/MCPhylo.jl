02330d80858bb38c1b279968bd6efe30f1efbce6
ansable <anchous12@gmail.com>
2020-09-02 13:54:00 +0200

    SPR non binary

--- src/Tree/Tree_SPR.jl
+++ src/Tree/Tree_SPR.jl
@@ -1,3 +1,6 @@
+include("../MCPhylo.jl")
+include("Parser/ParseNewick.jl")
+#include("./Node_Type.jl")
 ##iterator function:
 #input is input tree
 #list of nodes used(LoNU) = []
@@ -28,6 +31,44 @@
 # addchild(dest.mother,TtM)
 #
 
-function transform(root::T, TtM::T, dest::T) where T<:AbstractNode
-    motherofTtM = TtM.mother
-    
+# function transform(root::T, TtM::T, dest::T) where T<:AbstractNode
+#     motherofTtM = TtM.mother
+#
+
+function SPR(root::T)::T where T <: AbstractNode
+#TODO: BINARY TREES
+#TODO: HOW MANY ITERATIONS??
+#TODO: IF LENGTH IS SMALLER, THAT'S NEW ORIGINAL TREE
+
+    original_tree_length = tree_length(root)
+    subtree_root, nodes_of_subtree = create_random_subtree(root)
+    root_tree_with_no_subtree = prune_tree(root,nodes_of_subtree)
+    spr_tree = merge_randomly(root_tree_with_no_subtree,subtree_root)
+    spr_length = tree_length(spr_tree)
+    if spr_length < original_tree_length
+        println("CONGRATS")
+        println("Original was",original_tree_length)
+        println("Now it's",spr_length)
+        SPR(spr_tree)
+    else
+        println("Original was",original_tree_length)
+        println("Now it's",spr_length)
+        SPR(root)
+end # if else
+end
+
+function create_random_subtree(root::T)::T  where T<:AbstractNode
+    subtree_root = random_node(root)
+    #TODO: check how to compare
+    while subtree_root == root
+        subtree_root = random_node(root)
+    end #while
+    nodes_of_subtree = post_order(subtree_root)
+    return subtree_root, nodes_of_subtree
+end #function
+
+function merge_randomly(root::T,subtree_root::T)::T  where T<:AbstractNode
+    random_mother = random_node(root)
+    add_child!(random_mother,subtree_root)
+    return root
+end #function
