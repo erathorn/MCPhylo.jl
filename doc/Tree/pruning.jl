116d9e097861e7c723da9e1e58e8b6b48edde95c
Steinar <46631699+Arcumenn@users.noreply.github.com>
2020-09-07 08:06:10 +0200

    Pruning fix (#10)
    * update pruning test
    
    * fix pruning to check if multiple nodes have the same name

--- doc/Tree/pruning.jl
+++ doc/Tree/pruning.jl
@@ -2,48 +2,36 @@ include("../../src/MCPhylo.jl")
 using .MCPhylo
 using Test
 
-tree = MCPhylo.parsing_newick_string("(A,B,(C,D)E)F;")
+tree = MCPhylo.parsing_newick_string("(A,B,(C,(D,E)F)G)H;")
 MCPhylo.number_nodes!(tree)
 
-@testset "Pruning leave"
-
-    node_list2 = ["C"]
-    prune_tree2 = MCPhylo.prune_tree(tree, node_list2)
-
-    @test newick(prune_tree2) ==  newick(MCPhylo.parsing_newick_string("(A,B)F;"))
-
-
+@testset "Pruning leaf" begin
+    node_list = ["C"]
+    prune_tree = MCPhylo.prune_tree(tree, node_list)
+    @test newick(prune_tree) == newick(MCPhylo.parsing_newick_string("(A,B,((D,E)F)G)H;"))
 end
 
-
-@testset "Pruning root"
-    node_list1 = ["F"]
-
-    prune_tree1 = MCPhylo.prune_tree(tree, node_list1)
-
-    @test newick(prune_tree1) ==  newick(MCPhylo.parsing_newick_string("(A,(D)E)F;"))
+@testset "Pruning root" begin
+    node_list = ["H"]
+    @test_throws ArgumentError MCPhylo.prune_tree(tree, node_list)
 end
 
-@testset "Pruning misc"
+@testset "Pruning misc" begin
     node_list1 = ["B", "C"]
-    node_list2 = [ "E"]
-    node_list3 = ["A", "C", "E"]
+    node_list2 = ["F"]
+    node_list3 = ["A", "C", "G"]
 
     prune_tree1 = MCPhylo.prune_tree(tree, node_list1)
     prune_tree2 = MCPhylo.prune_tree(tree, node_list2)
     prune_tree3 = MCPhylo.prune_tree(tree, node_list3)
 
-
-    @test newick(prune_tree1) ==  newick(MCPhylo.parsing_newick_string("(A,(D)E)F;"))
-    @test newick(prune_tree2) ==  newick(MCPhylo.parsing_newick_string("(A,B)F;"))
-    @test newick(prune_tree3) ==  newick(MCPhylo.parsing_newick_string("(B)F;"))
-
+    @test newick(prune_tree1) == newick(MCPhylo.parsing_newick_string("(A,((D,E)F)G)H;"))
+    @test newick(prune_tree2) == newick(MCPhylo.parsing_newick_string("(A,B,(C)G)H;"))
+    @test newick(prune_tree3) == newick(MCPhylo.parsing_newick_string("(B)H;"))
 end
 
-
-@testset "Pruning inplace"
+@testset "Pruning inplace" begin
     node_list1 = ["B", "C"]
-
     MCPhylo.prune_tree!(tree, node_list1)
-    @test newick(tree) ==  newick(MCPhylo.parsing_newick_string("(A,(D)E)F;"))
+    @test newick(tree) ==  newick(MCPhylo.parsing_newick_string("(A,((D,E)F)G)H;"))
 end
--- src/Tree/Tree_Pruning.jl
+++ src/Tree/Tree_Pruning.jl
@@ -1,26 +1,4 @@
 """
-    prune_tree!(root::T, node_names::Vector{String})
-
-This function removes specific nodes and their daughters from a tree
-"""
-function prune_tree!(root::T, node_names::Vector{String})::Nothing where T<:AbstractNode
-    if root.name in node_names
-        # delete entire tree if root node needs to be deleted
-        root = nothing
-    else
-        for child in root.children
-            if child.name in node_names
-                remove_child!(root, child)
-            end
-        end
-        # recursively check all nodes of the tree
-        for child in root.children
-            prune_tree!(child,node_names)
-        end
-    end
-end
-
-"""
     prune_tree(root::T, node_names::Vector{String})::T where T<:AbstractNode
 
 This function returns a copy of a tree with specific nodes including their
@@ -31,4 +9,60 @@ function prune_tree(root::T, node_names::Vector{String})::T where T<:AbstractNod
     copyroot = deepcopy(root)
     prune_tree!(copyroot, node_names)
     return copyroot
+end
+
+
+"""
+    prune_tree(root::T, node_names::Vector{String})::T where T<:AbstractNode
+
+This function returns a copy of a tree with specific nodes including their
+descendants removed
+"""
+function prune_tree(root::T, nodes::Vector{T})::T where T<:AbstractNode
+    # copy the tree and call the inplace version of the function on the copy
+    copyroot = deepcopy(root)
+    prune_tree!(copyroot, nodes)
+    return copyroot
+end
+
+
+"""
+    prune_tree!(root::T, node_names::Vector{String})::T where T<:AbstractNode
+
+In-place version of prune_tree
+"""
+function prune_tree!(root::T, node_names::Vector{String})::Nothing where T<:AbstractNode
+    nodes = post_order(root)
+    names = [node.name for node in nodes]
+    nodes_to_prune = Vector{Node}()
+    for name in node_names
+        indeces = findall(x->x == name, names)
+        if length(indeces) > 1
+            throw(ArgumentError("Multiple nodes are named \"$name\""))
+        elseif length(indeces) == 0
+            print("Warning: No node named \"$name\" in tree")
+        else
+            push!(nodes_to_prune, nodes[indeces[1]])
+        end # if/else
+    end # for
+    if length(nodes_to_prune) == 0
+        throw(ArgumentError("None of the node names correspond to a node in the tree"))
+    end # if
+    prune_tree!(root, nodes_to_prune)
+end
+
+
+"""
+    prune_tree!(root::T, node_names::Vector{String})
+
+In-place version of prune_tree
+"""
+function prune_tree!(root::T, nodes::Vector{T})::Nothing where T<:AbstractNode
+    if root in nodes
+        throw(ArgumentError("trying to prune root, please set root to nothing instead"))
+    else
+        for node in nodes
+            remove_child!(node.mother, node)
+        end
+    end
 end
