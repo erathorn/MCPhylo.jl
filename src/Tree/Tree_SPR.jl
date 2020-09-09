c1ffda14142a8b0543539fcc3f60e79f9b05444d
ansable <anchous12@gmail.com>
2020-09-09 20:30:05 +0200

    functions updated

--- src/Tree/Tree_SPR.jl
+++ src/Tree/Tree_SPR.jl
@@ -1,53 +1,51 @@
-##iterator function:
-#input is input tree
-#list of nodes used(LoNU) = []
-#list of trees(LoT) = [input tree]
-#while LoT not empty:
-#curtree = LoT.pop
-#copyoftree = curtree
-#for node x in [all nodes in copyoftree]
-#dest = random node from copyoftree (maybe keep track of this somehow 4speed)
-#newtree = transformfunction(copyoftree,x, dest)
-#copyoftree = curtree
-# if newtree.length() < curtree.length()
-#LoT.add(newtree)
-#
-#outside while loop: return curtree
-#
-
-
-##tree transforming function:
-#input is root tree, thing to move(TtM), and destination(dest)
-# removechild(mother of TtM, TtM)
-#IF BINARY:
-# make new node, addchild(newnode, TtM)
-# addchild(newnode, dest)
-#addchild(dest.mother, newnode)
-# removechild(dest.mother,dest)
-#IF NOT BINARY:
-# addchild(dest.mother,TtM)
-#
-
-# function transform(root::T, TtM::T, dest::T) where T<:AbstractNode
-#     motherofTtM = TtM.mother
-#
 #TODO: BINARY TREES
-#TODO: HOW MANY ITERATIONS??
-#TODO: IF LENGTH IS SMALLER, THAT'S NEW ORIGINAL TREE
+#TODO: why can not use prune_tree
+function SPR(original_root::Node)::AbstractNode
+    #TODO: HOW MANY ITERATIONS??
+    root = deepcopy(original_root)
+    spr_tree = perform_spr(root)
+    spr_length = tree_length(spr_tree)
+    original_tree_length = tree_length(original_root)
 
-function SPR(root::Node)::AbstractNode
+    if spr_length < original_tree_length
+        println("GOOD ONE")
+        println("Newick Representation of SMALLER TREE")
+        println(newick(spr_tree))
+        return root
+        #perform_spr(spr_tree)
+    else
+        println("BAD ONE")
+        println("before we had this length ", original_tree_length)
+        println("and we got this one ",spr_length)
+        println("Newick Representation of bigger tree which we got")
+        println(newick(spr_tree))
+        return root
+end #ifelse
+    #TODO: IF LENGTH IS SMALLER, THAT'S NEW ORIGINAL TREE
+end
+
+function perform_spr(root::Node)::AbstractNode
+    subtree_root, nodes_of_subtree = create_random_subtree(root)
+    remove_child!(subtree_root.mother,subtree_root)
+    spr_tree = merge_randomly(root,subtree_root)
+    return spr_tree
+end
+
+function test_perform_spr(root::Node)::AbstractNode
     println("BEGINNING TREE OF THIS LOOP: ")
     println(newick(root))
     original_tree_length = tree_length(root)
     subtree_root, nodes_of_subtree = create_random_subtree(root)
     names_of_subtree = [i.name for i in nodes_of_subtree]
     println("THE NAMES TO REMOVE ARE ", names_of_subtree)
-    root_tree_with_no_subtree = prune_tree(root,names_of_subtree)
+    #root_tree_with_no_subtree = prune_tree(root,nodes_of_subtree)
+    remove_child!(subtree_root.mother,subtree_root)
     println("PRUNED TREE OF THIS ROOT: ")
-    println(newick(root_tree_with_no_subtree))
+    #println(newick(root_tree_with_no_subtree))
+    println(newick(root))
     # println("THIS SHOULD NOT HAVE PART")
     # println(newick(root_tree_with_no_subtree))
-    spr_tree = merge_randomly(root_tree_with_no_subtree,subtree_root)
+    spr_tree = merge_randomly(root,subtree_root)
     spr_length = tree_length(spr_tree)
     println("SUBTREE OF THIS LOOP: ")
     println(newick(subtree_root))
@@ -58,23 +56,6 @@ function SPR(root::Node)::AbstractNode
     println("RESULT LENGTH: ")
     println(spr_length)
     return spr_tree
-    # if spr_length < original_tree_length
-    #     println("WENT TO THE SMALLER TREE")
-    #     println("ORIGINAL LENGTH ", original_tree_length)
-    #     println("ACHIEVED SMALLER LENGTH ", spr_length)
-    #     println()
-    #     println()
-    #     println()
-    #     println("Newick Representation of SMALLER TREE")
-    #     println(newick(spr_tree))
-    #     SPR(spr_tree)
-    # else
-    #     ("STARTED AGAIN, LENGTH NOT SMALLER")
-    #     println("before we had this length ", original_tree_length)
-    #     println("and we got this one ",spr_length)
-    #     println("Newick Representation of bigger tree which we got")
-    #     println(newick(spr_tree))
-    #     SPR(root)
 
 end
 
