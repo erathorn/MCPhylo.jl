715b2c615fb9559c9ea7bdc02ba0f43129b7f586
arcumenn <steinar.graessel@gmx.net>
2020-11-26 13:09:44 +0100

    include loose test in the big consensus test file

--- doc/Tree/consensus.jl
+++ doc/Tree/consensus.jl
@@ -1,4 +1,5 @@
-using MCPhylo
+include("../../src/MCPhylo.jl")
+using .MCPhylo
 using Test
 
 @testset "find_common_clusters" begin
@@ -141,4 +142,37 @@ end
     tree4 = MCPhylo.parsing_newick_string("(((B,C),A),D,F)")
     push!(trees, tree4)
     @test_throws ArgumentError MCPhylo.majority_consensus_tree(trees)
+
+    tree1 = MCPhylo.parsing_newick_string("(((A,B)AB,C)ABC,(D,E)DE)no_name;")
+    tree2 = MCPhylo.parsing_newick_string("((A,C)AC,(B,D,E)BDE)no_name;")
+    tree3 = MCPhylo.parsing_newick_string("(((B,C)BC,A)BCA,D,E)no_name;")
+    trees = [tree1, tree2, tree3]
+    MCPhylo.number_nodes!.(trees)
+    MCPhylo.set_binary!.(trees)
+    result = newick(MCPhylo.parsing_newick_string("((A,B,C),D,E)"))
+    @test newick(MCPhylo.majority_consensus_tree(trees)) == result
+end
+
+@testset "loose_consensus_tree" begin
+    tree1 = MCPhylo.parsing_newick_string("(((A,B),C),(D,E))")
+    tree2 = MCPhylo.parsing_newick_string("((A,C),(B,D,E))")
+    tree3 = MCPhylo.parsing_newick_string("(((B,C),A),D,E)")
+    trees = [tree1, tree2, tree3]
+    MCPhylo.number_nodes!.(trees)
+    MCPhylo.set_binary!.(trees)
+    result = newick(MCPhylo.parsing_newick_string("(A,B,C,(D,E))"))
+    @test newick(MCPhylo.loose_consensus_tree(trees)) == result
+
+    tree4 = MCPhylo.parsing_newick_string("(((B,C),A),D,F)")
+    push!(trees, tree4)
+    @test_throws ArgumentError MCPhylo.loose_consensus_tree(trees)
+
+    tree1 = MCPhylo.parsing_newick_string("(((A,B)AB,C)ABC,(D,E)DE)no_name;")
+    tree2 = MCPhylo.parsing_newick_string("((A,C)AC,(B,D,E)BDE)no_name;")
+    tree3 = MCPhylo.parsing_newick_string("(((B,C)BC,A)BCA,D,E)no_name;")
+    trees = [tree1, tree2, tree3]
+    MCPhylo.number_nodes!.(trees)
+    MCPhylo.set_binary!.(trees)
+    result = newick(MCPhylo.parsing_newick_string("(A,B,C,(D,E)DE)"))
+    @test newick(MCPhylo.loose_consensus_tree(trees)) == result
 end
