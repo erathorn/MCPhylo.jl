87291fd8bb4112ee34ddb0319d819b1feeb26a1a
Arcumenn <steinar.graessel@gmx.net>
2020-12-05 10:20:11 +0100

    further docstring additions

--- src/Tree/Tree_Consensus.jl
+++ src/Tree/Tree_Consensus.jl
@@ -417,10 +417,12 @@ end
 """
     majority_consensus_tree(trees::Vector{T}, percentage::Float64=0.5)::T where T<:AbstractNode
 
-Construct the majority rule consensus tree from a set of trees. By default the
-output tree includes clusters that occur in over 50% of the trees. This can be
-customized when calling the function. The algorithm is based on section 3 of the
-paper and uses all functions above (pseudocode on page 28:12 of the paper)
+Construct the majority rule consensus tree from a set of trees that share the
+same leafset. By default the output tree includes clusters that occur in over
+50% of the trees. This can be customized when calling the function. The function
+returns the root node of the majority consensus tree, from which it can be
+traversed. The algorithm is based on section 3 of the paper and uses all
+functions above (pseudocode on page 28:12 of the paper).
 """
 function majority_consensus_tree(trees::Vector{T}, percentage::Float64=0.5)::T where T<:AbstractNode
     merged_tree = deepcopy(trees[1])
@@ -517,7 +519,9 @@ end
 
 Construct the loose consensus tree from a set of trees that share the same
 leafset. I.e. a tree with all the clusters that appear in at least one tree
-and are compatible with all trees. Based on section 4 of the paper.
+and are compatible with all trees. Returns the root node of the loose consensus
+tree, from which it can be traversed. This algorithm is based on section 4 of
+the paper (pseudocode on page 28:15).
 """
 function loose_consensus_tree(trees::Vector{T})::T where T<:AbstractNode
     trees_copy = deepcopy(trees)
