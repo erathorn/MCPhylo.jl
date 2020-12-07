d1e7dbd947064397e078c7a85c997b00784720ad
Steinar <46631699+Arcumenn@users.noreply.github.com>
2020-12-07 16:50:00 +0100

    Loose consensus
    Based on: 
    Jesper Jansson, Chuanqi Shen, and Wing-Kin Sung. 2016. Improved algorithms
    for constructing consensustrees. J. ACM 63, 3, Article 28 (June 2016), 24 pages
    https://dl.acm.org/doi/pdf/10.1145/2925985
    
    Co-authored-by: Tina Hafner <tina.hafner@student.uni-tuebingen.de>
    Co-authored-by: erathorn <erathorn@users.noreply.github.com>
    Co-authored-by: Arcumenn <steinr.graessel@gmx.net>

--- doc/Tree/consensus.jl
+++ doc/Tree/consensus.jl
@@ -62,6 +62,14 @@ end
     @test MCPhylo.newick(MCPhylo.one_way_compatible(tree, tree2)) == MCPhylo.newick(expected_tree)
 end
 
+@testset "get_leaf_ranks" begin
+    ref_tree = MCPhylo.parsing_newick_string("((A,(B,(C,(D,E)))),(F,(G,H)))")
+    nodes = post_order(ref_tree)
+    @test MCPhylo.get_leaf_ranks(nodes) == Dict([("A", 1), ("B", 2), ("C", 3),
+                                                 ("D", 4), ("E", 5), ("F", 6),
+                                                 ("G", 7), ("H", 8)])
+end
+
 @testset "order_tree!" begin
     tree = MCPhylo.parsing_newick_string("(A,B,(C,(D,E)F)G)H;")
     MCPhylo.number_nodes!(tree)
@@ -141,4 +149,46 @@ end
     tree4 = MCPhylo.parsing_newick_string("(((B,C),A),D,F)")
     push!(trees, tree4)
     @test_throws ArgumentError MCPhylo.majority_consensus_tree(trees)
+
+    tree1 = MCPhylo.parsing_newick_string("(((A,B)AB,C)ABC,(D,E)DE)no_name;")
+    tree2 = MCPhylo.parsing_newick_string("((A,C)AC,(B,D,E)BDE)no_name;")
+    tree3 = MCPhylo.parsing_newick_string("(((B,C)BC,A)BCA,D,E)no_name;")
+    trees = [tree1, tree2, tree3]
+    MCPhylo.number_nodes!.(trees)
+    MCPhylo.set_binary!.(trees)
+    result = newick(MCPhylo.parsing_newick_string("((A,B,C),D,E)"))
+    @test newick(MCPhylo.majority_consensus_tree(trees)) == result
 end
+
+@testset "loose_consensus_tree" begin
+    tree1 = MCPhylo.parsing_newick_string("(((A,B),C),(D,E))")
+    tree2 = MCPhylo.parsing_newick_string("((A,C),(B,D,E))")
+    tree3 = MCPhylo.parsing_newick_string("(((B,C),A),D,E)")
+    trees = [tree1, tree2, tree3]
+    MCPhylo.number_nodes!.(trees)
+    MCPhylo.set_binary!.(trees)
+    result = newick(MCPhylo.parsing_newick_string("(A,B,C,(D,E))"))
+    @test newick(MCPhylo.loose_consensus_tree(trees)) == result
+
+    tree4 = MCPhylo.parsing_newick_string("(((B,C),A),D,F)")
+    push!(trees, tree4)
+    @test_throws ArgumentError MCPhylo.loose_consensus_tree(trees)
+
+    tree1 = MCPhylo.parsing_newick_string("(((A,B)AB,C)ABC,(D,E)DE)no_name;")
+    tree2 = MCPhylo.parsing_newick_string("((A,C)AC,(B,D,E)BDE)no_name;")
+    tree3 = MCPhylo.parsing_newick_string("(((B,C)BC,A)BCA,D,E)no_name;")
+    trees = [tree1, tree2, tree3]
+    MCPhylo.number_nodes!.(trees)
+    MCPhylo.set_binary!.(trees)
+    result = newick(MCPhylo.parsing_newick_string("(A,B,C,(D,E)DE)"))
+    @test newick(MCPhylo.loose_consensus_tree(trees)) == result
+end
+
+
+"""
+trees = MCPhylo.ParseNewick("./doc/Tree/Drav_mytrees_1.nwk")
+MCPhylo.set_binary!.(trees)
+MCPhylo.number_nodes!.(trees)
+majority_tree = MCPhylo.majority_consensus_tree(trees)
+println(newick(majority_tree))
+"""
