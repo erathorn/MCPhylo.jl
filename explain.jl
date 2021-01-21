34c587ec678f64acb55418dce7d7c9c3df9280a0
Arcumenn <steinar.graessel@gmx.net>
2021-01-21 16:02:02 +0100

    add explain file back, for testing

---
+++ explain.jl
@@ -0,0 +1,41 @@
+include("./src/MCPhylo.jl")
+using .MCPhylo
+using Serialization
+
+
+"""
+Mamba Julia
+Tutorial
+
+"""
+
+
+data = rand(Normal(0,1), 5000)
+
+my_data=Dict(:data=>data)
+
+model = Model(
+     data = Stochastic(1, (μ, σ) -> Normal(μ, σ), false),
+        μ = Stochastic(()->Normal(),true),
+        σ = Stochastic(()->Exponential(1), true)
+)
+
+inits = [Dict(:data => data,
+             :μ => randn(),
+             :σ => rand()),
+        Dict(:data => data,
+            :μ => randn(),
+            :σ => rand())]
+
+samplers = [NUTS(:μ),
+            Slice(:σ, 0.1)]
+
+setsamplers!(model, samplers)
+
+sim = mcmc(model, my_data, inits, 5000, burnin=500, thin=5, chains=2)
+
+plot(sim, [:bar, :autocor]; fmt=:pdf, nrow=2, ncol=1)
+
+gelmandiag(sim)
+
+describe(sim)
