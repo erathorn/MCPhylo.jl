73d5c35eafc8ca7a4ebddf5c774c46e6fda663a7
Arcumenn <steinar.graessel@gmx.net>
2021-02-03 15:38:18 +0100

    delete remnants of integration Phylo package for plotting

--- src/output/explain.jl
+++ src/output/explain.jl
@@ -3,5 +3,4 @@ using .MCPhylo
 
 trees = MCPhylo.ParseNewick("./doc/Tree/Drav_mytrees_1.nwk")
 
-plot(trees[1], :fan, msc=:green)
-plot("./doc/Tree/Drav_mytrees_1.nwk", msc=:purple)
+Plots.plot(trees[1], :fan, msc=:green)
