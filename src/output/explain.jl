84a0c58596c80a7c41eefcb408382d44f3baf0ed
Arcumenn <steinar.graessel@gmx.net>
2021-02-07 14:01:02 +0100

    fix fan graph and add basic example to explain.jl

--- src/output/explain.jl
+++ src/output/explain.jl
@@ -3,4 +3,6 @@ using .MCPhylo
 
 trees = MCPhylo.ParseNewick("./doc/Tree/Drav_mytrees_1.nwk")
 
-plot1 = Plots.plot(trees[1], showtips=true, msc=:green, mc=:yellow, lc=:white, bg=:black, tipfont=(7, :white))
+Plots.plot(trees[1])
+Plots.plot(trees[1], treetype=:fan, msc=:blue, mc=:yellow, lc=:white,
+           bg=:black, tipfont=(7, :lightgreen))
