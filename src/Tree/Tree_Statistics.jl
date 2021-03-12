7809d3d00902fb4287458d42f8f738f4316c9a3e
Arcumenn <steinar.graessel@gmx.net>
2021-03-12 18:09:16 +0100

    add asdsf for arrays of trees; add testing example

--- src/Tree/Tree_Statistics.jl
+++ src/Tree/Tree_Statistics.jl
@@ -1,4 +1,4 @@
-function parse_and_number(treestring::S)::Node where S <: AbstractString
+function parse_and_number(treestring::S)::Node where S<:AbstractString
     p_tree2 = parsing_newick_string(string(treestring))
     set_binary!(p_tree2)
     number_nodes!(p_tree2)
@@ -34,23 +34,13 @@ end # nodnums2names
 Calculate the average standard deviation of split frequencies for two files
 containing newick representations of trees.
 """
-function ASDSF(args...; freq::Int64=1, check_leafsets::Bool=true
+function ASDSF(args...; freq::Int64=1, check_leaves::Bool=true
               )::Vector{Float64}
 
-    splitsQueue = Accumulator{Tuple{Set{String}, Set{String}}, Int64}()
-    splitsQueues = Vector{Accumulator{Tuple{Set{String}, Set{String}}, Int64}}()
-    for arg in args
-        push!(splitsQueues, Accumulator{Tuple{Set{String}, Set{String}}, Int64}())
-    end
-    ASDF_vals = zeros(Int(countlines(args[1])/freq))
-    all_keys = Set{Tuple{Set{String},Set{String}}}()
-    run = 1
-    p = Progress(length(ASDF_vals))
-
-    for (i, lines) in enumerate(zip([eachline(arg) for arg in args]...))
+    function asdsf_int(i, lines)
         if mod(i, freq) == 0
             trees = [parse_and_number(tree_string) for tree_string in lines]
-            check_leafsets && check_leafsets(trees)
+            check_leaves && check_leafsets(trees)
             bps = [get_bipartitions(tree) for tree in trees]
             named_bps = [nodnums2names(bp, tree) for (bp, tree) in zip(bps, trees)]
             cmds = [countmap(named_bp) for named_bp in named_bps]
@@ -78,60 +68,37 @@ function ASDSF(args...; freq::Int64=1, check_leafsets::Bool=true
             run += 1
             ProgressMeter.next!(p)
         end # if
-    end # for
-    ASDF_vals
-end # ASDSF
-
-
-"""
-    ASDSF(args...; freq::Int64=1, check_leafsets::Bool=true)::Vector{Float64}
-
-Calculate the average standard deviation of split frequencies for two files
-containing newick representations of trees.
-"""
-function ASDSF(args...; freq::Int64=1, check_leafsets::Bool=true
-              )::Vector{Float64}
+    end # asdsf_int
 
     splitsQueue = Accumulator{Tuple{Set{String}, Set{String}}, Int64}()
     splitsQueues = Vector{Accumulator{Tuple{Set{String}, Set{String}}, Int64}}()
     for arg in args
         push!(splitsQueues, Accumulator{Tuple{Set{String}, Set{String}}, Int64}())
-    end
-    ASDF_vals = zeros(Int(countlines(args[1])/freq))
+    end # for
     all_keys = Set{Tuple{Set{String},Set{String}}}()
     run = 1
-    p = Progress(length(ASDF_vals))
-    for (i, lines) in enumerate(zip([eachline(arg) for arg in args]...))
-        if mod(i, freq) == 0
-            trees = [parse_and_number(tree_string) for tree_string in lines]
-            check_leafsets && check_leafsets(trees)
-            bps = [get_bipartitions(tree) for tree in trees]
-            named_bps = [nodnums2names(bp, tree) for (bp, tree) in zip(bps, trees)]
-            cmds = [countmap(named_bp) for named_bp in named_bps]
-            outer = 0.0
-            new_splits = union([keys(cmds[t]) for t in 1:length(trees)]...)
-            all_keys = union(all_keys, new_splits)
-            for split in new_splits
-                inc!(splitsQueue, split)
-            end # for
-            for split in all_keys
-                inner = 0.0
-                for (t, tree) in enumerate(trees)
-                    if haskey(cmds[t], split)
-                        inner += 1 / length(trees)
-                        inc!(splitsQueues[t], split)
-                    end # if
-                end # for
-                if any([x[split] / run > 0.1 for x in splitsQueues])
-                    fre = splitsQueue[split] / run
-                    inner += (inner - fre) ^ 2
-                    outer += sqrt(inner)
-                end # if
-            end # for
-            ASDF_vals[run] = outer / length(splitsQueue)
-            run += 1
-            ProgressMeter.next!(p)
-        end # if
-    end # for
+    if all(x-> typeof(x) == Vector{String}, args)
+        ASDF_vals = zeros(Int(length(args[1]) / freq))
+        args_joined = Vector{String}()
+        for arg in args
+            push!(args_joined, join(arg, "\n"))
+        end # for
+        p = Progress(length(ASDF_vals))
+        for (i, lines) in enumerate(zip([eachline(IOBuffer(arg)) for arg in args_joined]...))
+            asdsf_int(i, lines)
+        end # for
+    elseif all(x-> typeof(x) == String, args)
+        ASDF_vals = zeros(Int(countlines(args[1]) / freq))
+        p = Progress(length(ASDF_vals))
+        for (i, lines) in enumerate(zip([eachline(arg) for arg in args]...))
+            asdsf_int(i, lines)
+        end # for
+    else
+        throw(ArgumentError("Input needs to be filenames as strings, or arrays
+                            of newick strings"))
+    end # if/else
     ASDF_vals
+
+
+
 end # ASDSF
