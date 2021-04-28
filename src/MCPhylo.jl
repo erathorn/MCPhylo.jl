4b8f713e7868d71830e0d7f0c14d47ac704b2b55
eratorn <wahle.joh@gmail.com>
2021-04-28 15:12:43 +0200

    properly export

--- src/MCPhylo.jl
+++ src/MCPhylo.jl
@@ -444,7 +444,7 @@ export
   RWM, RWMVariate,
   Slice, SliceMultivariate, SliceUnivariate,
   SliceSimplex, SliceSimplexVariate,
-  DMH, DMHVariate
+  DMH, DMHVariate,
   PNUTS, PNUTSVariate,
   Empirical, EmpiricalVariate
 
