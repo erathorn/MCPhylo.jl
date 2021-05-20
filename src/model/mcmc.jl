25ca5e31117b9e412dc3a3e87d24de19d7c0cd2b
Arcumenn <steinar.graessel@gmx.net>
2021-05-20 14:46:46 +0200

    adapt for new structure using pmap again

--- src/model/mcmc.jl
+++ src/model/mcmc.jl
@@ -189,6 +189,7 @@ function mcmc_worker!(args::AbstractArray, ASDSF_step::Int64=0,
   end # for
   mv = samparas(m)
   sim.moves[1] = mv
+  println("Chain finished")
   (sim, m, ModelState(unlist(m), gettune(m)))
 end # mcmc_worker!
 
