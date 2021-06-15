e5f29a712e3f2e975d482cea599c8f07307c3331
Arcumenn <steinar.graessel@gmx.net>
2021-06-15 11:25:26 +0200

    change structure for exc type constraints

--- src/distributions/TreeConstraints.jl
+++ src/distributions/TreeConstraints.jl
@@ -1,24 +1,27 @@
 """
-    generate_constraints(; mono::Vector{Vector{String}}=Vector{String}[],
-                         not_mono::Vector{Vector{String}}=Vector{String}[],
-                         exc::Vector{Vector{String}}=Vector{String}[]
-                         )::Dict{Symbol, Vector{Vector{String}}}
+    generate_constraints(
+        ; mono::Vector{Vector{S}}=Vector{String}[],
+        not_mono::Vector{Vector{S}}=Vector{String}[],
+        exc::Vector{Tuple{Vector{S}, Vector{S}}}=Vector{Tuple{Vector{String}, Vector{String}}}[]
+    )::Dict{Symbol, Union{Vector{Vector{S}}, Vector{Tuple{Vector{S}, Vector{S}}}}}
+        where S<:AbstractString
 
 Generate a dictionary of constraints based on the given arguments.
 Mono for all minophyletic groups. not_mono for leafs that are not allowed
 to form a monophletic group. And exc for a partial constraint, where one or more
-leafs are not allowed to be part of a specific clade. The excluded leafs are
-separated from the clade they are not a part of via a colon entry in the array.
+leafs are not allowed to be part of a specific clade.
 
 e.g.: generate_constraints(mono=[["a", "b"], ["a", "c"]],
-                           exc=["b", "c", ":", "d"])
+                           exc=[(["b", "c"],["d"]])
 """
-function generate_constraints(; mono::Vector{Vector{String}}=Vector{String}[],
-                              not_mono::Vector{Vector{String}}=Vector{String}[],
-                              exc::Vector{Vector{String}}=Vector{String}[]
-                              )::Dict{Symbol, Vector{Vector{String}}}
+function generate_constraints(
+    ; mono::Vector{Vector{S}}=Vector{String}[],
+    not_mono::Vector{Vector{S}}=Vector{String}[],
+    exc::Vector{Tuple{Vector{S}, Vector{S}}}=Vector{Tuple{Vector{String}, Vector{String}}}[]
+)::Dict{
+    Symbol, Union{Vector{Vector{S}}, Vector{Tuple{Vector{S}, Vector{S}}}}} where S<:AbstractString
 
-    constraints_dict = Dict{Symbol, Vector{Vector{String}}}()
+    constraints_dict = Dict{Symbol, Union{Vector{Vector{S}}, Vector{Tuple{Vector{S}, Vector{S}}}}}()
     constrainttypes = [:mono, :not_mono, :exc]
     constraints_dict[:mono] = unique(mono)
     constraints_dict[:not_mono] = unique(not_mono)
@@ -28,20 +31,22 @@ end # generate_constraints
 
 
 """
-    generate_constraints!(constraints::Dict;
-                        mono::Vector{Vector{String}}=Vector{String}[],
-                        not_mono::Vector{Vector{String}}=Vector{String}[],
-                        exc::Vector{Vector{String}}=Vector{String}[]
-                        )::Dict{Symbol, Vector{Vector{String}}}
+    generate_constraints!(
+        constraints::Dict;
+        mono::Vector{Vector{S}}=Vector{String}[],
+        not_mono::Vector{Vector{S}}=Vector{String}[],
+        exc::Vector{Tuple{Vector{S}, Vector{S}}}=Vector{Tuple{Vector{String}, Vector{String}}}[]
+    )::Dict{Symbol, Union{Vector{Vector{S}}, Vector{Tuple{Vector{S}, Vector{S}}}}} where S<:AbstractString
 
 Function that adds further constraints to an existing dictionary of constraints.
 See basic generate_constraints function for more info.
 """
 function generate_constraints!(constraints::Dict;
-                               mono::Vector{Vector{String}}=Vector{String}[],
-                               not_mono::Vector{Vector{String}}=Vector{String}[],
-                               exc::Vector{Vector{String}}=Vector{String}[]
-                               )::Dict{Symbol, Vector{Vector{String}}}
+                               mono::Vector{Vector{S}}=Vector{String}[],
+                               not_mono::Vector{Vector{S}}=Vector{String}[],
+                               exc::Vector{Tuple{Vector{S}, Vector{S}}}=Vector{Tuple{Vector{String}, Vector{String}}}[]
+                               )::Dict{Symbol, Union{Vector{Vector{S}}, Vector{Tuple{Vector{S}, Vector{S}}}}} where S<:AbstractString
+
 
     constraints2 = generate_constraints(mono=mono, not_mono=not_mono, exc=exc)
     # make sure entries are unique for each constraint category
@@ -61,7 +66,7 @@ specific format (each line one constraint), i.e.:
 mono        ,a,b,c
 mono        ,d,e
 not_mono    ,c,d
-exc         ,a,b,:,d
+exc         ,a,b:d,e
 
 See basic generate_constraints function for more info.
 """
@@ -79,7 +84,7 @@ Function that adds further constraints to an existing dictionary of constraints,
 based on a txt file with a specific format (each line one constraint). See basic
 generate_constraints function on a file for more info.
 """
-function generate_constraints!(constraints::Dict{Symbol, Vector{Vector{String}}}, filename::String)
+function generate_constraints!(constraints::Dict, filename::String)
     mono, not_mono, exc = parse_constraints(filename)
     generate_constraints!(constraints; mono=mono, not_mono=not_mono, exc=exc)
 end # generate_constraints!
@@ -91,15 +96,19 @@ end # generate_constraints!
 --- INTERNAL ---
 Helper function that reads constraints out of a file
 """
-function parse_constraints(filename::String)
-    mono, not_mono, exc = [Vector{String}[] for _ = 1:3]
+function parse_constraints(filename::String
+                          )::Tuple{Vector{Vector{S}}, Vector{Vector{S}}, Vector{Tuple{Vector{S}, Vector{S}}}} where S<:AbstractString
+
+    exc = Vector{Tuple{Vector{AbstractString}, Vector{AbstractString}}}()
+    mono, not_mono = [Vector{String}[] for _ = 1:2]
     for line in eachline(filename)
-        splitted_line = split(line, ",")
-        splitted_line[1] = strip(splitted_line[1])
-        splitted_line[1] == "mono" ? push!(mono, splitted_line[2:end]) :
-        splitted_line[1] == "not_mono" ? push!(not_mono, splitted_line[2:end]) :
-        splitted_line[1] == "exc" ? push!(exc, splitted_line[2:end]) :
-        @warn "Unsupported constraint type $(splitted_line[1]). Allowed types are 'mono', 'not_mono' and 'exc'"
+        line = split(line,":")
+        line = [split(x, ",") for x in line]
+        line[1][1] = strip(line[1][1])
+        line[1][1] == "mono" ? push!(mono, line[1][2:end]) :
+        line[1][1] == "not_mono" ? push!(not_mono, line[1][2:end]) :
+        line[1][1] == "exc" ? push!(exc, (line[1][2:end], line[2])) :
+        @warn "Unsupported constraint type $(line[1][1]). Allowed types are 'mono', 'not_mono' and 'exc'"
     end # for
     return (mono, not_mono, exc)
 end # parse_constraints
