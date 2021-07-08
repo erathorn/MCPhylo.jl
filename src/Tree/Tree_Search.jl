d8b3082612ebd6e54ba6e98b0fbf997b47ed7cde
Arcumenn <steinar.graessel@gmx.net>
2021-07-08 09:59:45 +0200

    remove the 'unsafe' warning

--- src/Tree/Tree_Search.jl
+++ src/Tree/Tree_Search.jl
@@ -35,12 +35,7 @@ end
 """
     find_num(root::T, num::Int64)  where T<:GeneralNode
 
-Find a node by its number. The function assumes that the node is present in the
-tree.
-
-Do not use this function if you are unsure whether the node is in the tree at all.
-
-Returns reference to Node.
+Find a node by its number. Returns reference to Node.
 
 * `root` : root Node of tree to be searched.
 
