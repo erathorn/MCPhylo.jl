3db46676d41c5da4b38819afd35dc8ac5cb87e80
eratorn <wahle.joh@gmail.com>
2021-12-10 10:24:48 +0100

    remove lines

--- src/MCPhylo.jl
+++ src/MCPhylo.jl
@@ -11,12 +11,9 @@ using Serialization
 using Distributed
 using Printf: @sprintf
 using LinearAlgebra
-#using Plots
 @reexport using Plots
 using Plots.PlotMeasures
-#using StatsPlots
 @reexport using StatsPlots
-#using MCPhyloTree
 @reexport using MCPhyloTree
 import MCPhyloTree: internal_external
 using RecipesBase
@@ -61,7 +58,6 @@ import Bijectors: link
 
 # include("distributions/pdmats2.jl")
 # using .PDMats2
-#include("Tree/Node_Type.jl") # We need this to get the Node type in
 
 #################### Types ####################
 
@@ -267,24 +263,9 @@ include("samplers/rwm.jl")
 include("samplers/slice.jl")
 include("samplers/slicesimplex.jl")
 
-# include("Tree/Tree_Basics.jl")
-# include("Tree/Converter.jl")
-# include("Tree/Tree_moves.jl")
-# include("Tree/Tree_Distance.jl")
-# include("Tree/Tree_Traversal.jl")
-# include("Tree/Tree_Search.jl")
-# include("Tree/Tree_Legacy.jl")
-# include("Tree/Tree_Clustering.jl")
-# include("Tree/Tree_Ladderizing.jl")
-# include("Tree/Tree_Pruning.jl")
-# include("Tree/Tree_Consensus.jl")
-# include("Tree/Tree_Statistics.jl")
-
-
 include("Parser/Parser.jl")
 include("Parser/ParseCSV.jl")
 include("Parser/ParseNexus.jl")
-#include("Parser/ParseNewick.jl")
 
 
 include("Utils/FileIO.jl")
@@ -396,56 +377,13 @@ export
 export
   make_tree_with_data,
   to_file, drop_samples,
-  # to_df,
-  # to_covariance,
-  # to_distance_matrix,
-  # node_distance,
-  # get_path,
-  #cut_samples,
-  # NNI!, NNI,
-  # SPR!, SPR,
-  # slide!, slide,
-  # swing!, swing,
-  # RF, randomize!,
-  # BHV_bounds,
-  # get_branchlength_vector,
-  # set_branchlength_vector!,
-  # post_order,
-  # pre_order,
-  # level_order,
-  # add_child!,
-  # remove_child!,
-  # delete_node!,
-  # insert_node!,
-  # find_lca,
-  # find_by_binary,
-  # find_by_name,
-  # find_num,
-  # find_name,
-  # find_root,
-  # create_tree_from_leaves,
-  # create_tree_from_leaves_cu,
-  # newick,
-  # tree_length,
-  # tree_height,
-  # path_length,
-  # get_sister,
-  # get_leaves,
-  # check_leafsets,
   generate_constraints,
   generate_constraints!,
   topological,
-  #neighbor_joining,
-  #upgma,
-  #prune_tree!, prune_tree,
-  #ladderize_tree!, ladderize_tree,
-  #majority_consensus_tree,
   discrete_gamma_rates,
   Restriction, JC, GTR, freeK,
   ASDSF
-  #, parsimony, ParseNewick,
-  #cov2tree
-
+  
 
 export
   cm,
