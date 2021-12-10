f125effb2f19267c7812bfcaa8d6c152e5cbd414
eratorn <wahle.joh@gmail.com>
2021-12-10 16:48:05 +0100

    proper capitalization

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,7 @@ makedocs(
     format = Documenter.HTML(),
     pages = [
         "Index" => "index.md",
-        "Likelihood" => "likelihood.md",
+        "Likelihood" => "likelihood1.md",
         "Distributions" => "distributions.md",
         "Model" => "model.md",
         "Output" => "output.md",
@@ -15,3 +15,9 @@ makedocs(
         "Utils" => "utils.md",
         "Links" => "links.md"]
    )
+
+deploydocs(
+    repo = "github.com/erathorn/MCPhylo.jl.git",
+    devbranch="main"
+     )
+
