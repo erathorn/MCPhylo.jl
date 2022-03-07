699121e6294dc331272acdc0b2bb1741e8a64a34
Tina Hafner <tina.hafner@student.uni-tuebingen.de>
2022-03-07 12:28:51 +0100

    add our tests to runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,10 +4,9 @@ using Random, Test
 include("utils.jl")
 
 
-const samplertests = [
-  #"abc",
-  "linear_model_uvp",
-  "linear_model_mvp"
+const likelihoodtests = [
+  "rates",
+  "substitutionmodels"
 ]
 
 const mcmctests = [
@@ -16,10 +15,24 @@ const mcmctests = [
 #   "readcoda"
 ]
 
+const modeltests = [
+  "test_dependent"
+]
+
+const outputtests = [
+  "test_chains"
+]
 
 const parsertests = [
   "csv",
-  "nexus"
+  "nexus",
+  "parser"
+]
+
+const samplertests = [
+  #"abc",
+  "linear_model_uvp",
+  "linear_model_mvp"
 ]
 
 println("Running tests:")
@@ -27,11 +40,11 @@ println("Running tests:")
 
 @testset "All tests" begin
 
-  # @testset "sampler tests" begin
-  #   for t in samplertests 
-  #     @runtest "samplers/" t
-  #   end
-  # end
+  @testset "likelihood tests" begin
+    for t in likelihoodtests
+      @runtest "likelihood/" t
+    end
+  end
 
   # @testset "mcmc tests" begin
   #   for t in mcmctests
@@ -39,10 +52,27 @@ println("Running tests:")
   #   end
   # end
 
+  @testset "model tests" begin
+    for t in modeltests
+      @runtest "model/" t
+    end
+  end
+
+  @testset "output tests" begin
+    for t in outputtests
+      @runtest "output/" t
+    end
+  end
+
   @testset "parser tests" begin
     for t in parsertests
       @runtest "parser/" t
     end
   end
 
+  # @testset "sampler tests" begin
+  #   for t in samplertests 
+  #     @runtest "samplers/" t
+  #   end
+  # end
 end
