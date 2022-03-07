88c54117f08a40ede4d019b73c462794519fcd82
Tina Hafner <tina.hafner@student.uni-tuebingen.de>
2022-03-07 12:27:58 +0100

    change file paths for runtests

--- test/parser/parser.jl
+++ test/parser/parser.jl
@@ -5,20 +5,20 @@
                 0.0 0.0 1.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0 1.0 0.0 0.0 1.0 0.0 0.0 0.0 0.0 0.0]
         dim2 = [1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 0.0 1.0 1.0 1.0; 
                 0.0 1.0 0.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0 0.0 1.0 1.0 0.0 0.0 1.0 0.0 0.0 0.0 1.0 1.0 1.0 0.0]
-        result = make_tree_with_data("./test/parser/example.csv", "csv", "-", "?", true)
+        result = make_tree_with_data("./parser/example.csv", "csv", "-", "?", true)
         @test result[2][:,:,1] == dim1
         @test result[2][:,:,2] == dim2
         @test result[1].nchild == 2
 
-        result = make_tree_with_data("./test/parser/example.csv", "csv", "-", "?", binary=false)
+        result = make_tree_with_data("./parser/example.csv", "csv", "-", "?", binary=false)
         @test result[1].nchild == 3
 
-        @test_throws FileSyntaxError make_tree_with_data("./test/parser/example.csv")
-        @test_throws ArgumentError make_tree_with_data("./test/parser/example.csv", "csv")
-        @test_throws ArgumentError make_tree_with_data("./test/parser/example.csv", "csv", "-")
+        @test_throws FileSyntaxError make_tree_with_data("./parser/example.csv")
+        @test_throws ArgumentError make_tree_with_data("./parser/example.csv", "csv")
+        @test_throws ArgumentError make_tree_with_data("./parser/example.csv", "csv", "-")
     end
 
     @testset "ParseNexus == ParseCSV" begin
-        @test MCPhylo.ParseNexus("./test/parser/example2.nex") == MCPhylo.ParseCSV("./test/parser/example.csv", "-", "?", false)
+        @test MCPhylo.ParseNexus("./parser/example2.nex") == MCPhylo.ParseCSV("./parser/example.csv", "-", "?", false)
     end
 end
