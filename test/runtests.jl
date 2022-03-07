196f8c2c1788bfe43a469e03a472b2a7a4046e04
Arcumenn <steinar.graessel@gmx.net>
2022-03-07 13:17:37 +0100

    add back all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -40,39 +40,39 @@ println("Running tests:")
 
 @testset "All tests" begin
 
-  @testset "likelihood tests" begin
-    for t in likelihoodtests
-      @runtest "likelihood/" t
+    @testset "likelihood tests" begin
+        for t in likelihoodtests
+            @runtest "likelihood/" t
+        end
     end
-  end
 
-  # @testset "mcmc tests" begin
-  #   for t in mcmctests
-  #     @runtest "mcmc/" t
-  #   end
-  # end
-
-  @testset "model tests" begin
-    for t in modeltests
-      @runtest "model/" t
+    @testset "mcmc tests" begin
+        for t in mcmctests
+            @runtest "mcmc/" t
+        end
     end
-  end
 
-  @testset "output tests" begin
-    for t in outputtests
-      @runtest "output/" t
+    @testset "model tests" begin
+        for t in modeltests
+            @runtest "model/" t
+        end
     end
-  end
 
-  @testset "parser tests" begin
-    for t in parsertests
-      @runtest "parser/" t
+    @testset "output tests" begin
+        for t in outputtests
+            @runtest "output/" t
+        end
     end
-  end
 
-  # @testset "sampler tests" begin
-  #   for t in samplertests 
-  #     @runtest "samplers/" t
-  #   end
-  # end
+    @testset "parser tests" begin
+        for t in parsertests
+            @runtest "parser/" t
+        end
+    end
+
+    @testset "sampler tests" begin
+        for t in samplertests 
+            @runtest "samplers/" t
+        end
+    end
 end
