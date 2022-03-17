b3bb064c2791e81c1b4d966ff3ad5bb45baed4a1
Arcumenn <steinar.graessel@gmx.net>
2022-03-17 11:41:24 +0100

    use index notation in chains test

--- test/output/chains.jl
+++ test/output/chains.jl
@@ -16,18 +16,18 @@
         @test getindex(c, :, :, [true, true, true]).value == v
 
         @testset "window2inds" begin
-            @test dropdims(getindex(c, 1, :, [true, true, true]).value, dims=1) == v[1, :, :]
-            @test getindex(c, 4:7, :, [true, true, true]).value == v[4:7, :, :]
-            @test getindex(c, 4:2:8, :, [true, true, true]).value == v[4:2:8, :, :]
+            @test dropdims(c[1, :, [true, true, true]].value, dims=1) == v[1, :, :]
+            @test c[4:7, :, [true, true, true]].value == v[4:7, :, :]
+            @test c[4:2:8, :, [true, true, true]].value == v[4:2:8, :, :]
             @test_throws ArgumentError getindex(c, "Iteration1", :, [true, true, true])
         end
 
         @testset "names2inds" begin
-            @test dropdims(getindex(c, :, 2, [true, true, true]).value, dims=2) == v[:,2,:]
-            @test dropdims(getindex(c, :, "Param1", [true, true, true]).value, dims=2) == v[:,1,:]
-            @test getindex(c, :, ["Param1", "Param2"], [true, true, true]).value == v
-            @test getindex(c, :, [true, true], [true, true, true]).value == v
-            @test getindex(c, :, [1, 2], [true, true, true]).value == v
+            @test dropdims(c[:, 2, [true, true, true]].value, dims=2) == v[:,2,:]
+            @test dropdims(c[:, "Param1", [true, true, true]].value, dims=2) == v[:,1,:]
+            @test c[:, ["Param1", "Param2"], [true, true, true]].value == v
+            @test c[:, [true, true], [true, true, true]].value == v
+            @test c[:, [1, 2], [true, true, true]].value == v
         end
 
         @testset "iters2inds" begin
