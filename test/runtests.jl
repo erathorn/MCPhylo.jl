f94ab5ea7aab503dc9ad78e9085d2c802478a258
erathorn <wahle.joh@gmail.com>
2022-04-05 09:15:13 +0200

    all tests activated

--- test/runtests.jl
+++ test/runtests.jl
@@ -50,29 +50,29 @@ println("Running tests:")
 
 @testset "All tests" begin
 
-    # @testset "distributions tests" begin
-    #     for t in distributionstests
-    #         @runtest "distributions/" t
-    #     end
-    # end
+    @testset "distributions tests" begin
+        for t in distributionstests
+            @runtest "distributions/" t
+        end
+    end
     
-    # @testset "likelihood tests" begin
-    #     for t in likelihoodtests
-    #         @runtest "likelihood/" t
-    #     end
-    # end
+    @testset "likelihood tests" begin
+        for t in likelihoodtests
+            @runtest "likelihood/" t
+        end
+    end
 
-    # @testset "mcmc tests" begin
-    #     for t in mcmctests
-    #         @runtest "mcmc/" t
-    #     end
-    # end
+    @testset "mcmc tests" begin
+        for t in mcmctests
+            @runtest "mcmc/" t
+        end
+    end
 
-    # @testset "model tests" begin
-    #     for t in modeltests
-    #         @runtest "model/" t
-    #     end
-    # end
+    @testset "model tests" begin
+        for t in modeltests
+            @runtest "model/" t
+        end
+    end
 
     @testset "output tests" begin
         for t in outputtests
@@ -80,15 +80,15 @@ println("Running tests:")
         end
     end
 
-    # @testset "parser tests" begin
-    #     for t in parsertests
-    #         @runtest "parser/" t
-    #     end
-    # end
+    @testset "parser tests" begin
+        for t in parsertests
+            @runtest "parser/" t
+        end
+    end
 
-    # @testset "sampler tests" begin
-    #     for t in samplertests 
-    #         @runtest "samplers/" t
-    #     end
-    # end
+    @testset "sampler tests" begin
+        for t in samplertests 
+            @runtest "samplers/" t
+        end
+    end
 end
