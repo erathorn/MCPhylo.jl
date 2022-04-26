0a4c4bcd00296fc2a67eedb335bf3bfeca902ea8
erathorn <wahle.joh@gmail.com>
2022-04-26 10:25:14 +0200

    some fixes

--- src/MCPhylo.jl
+++ src/MCPhylo.jl
@@ -188,7 +188,7 @@ struct ModelState
 end
 
 mutable struct Model
-    nodes::Dict{Symbol,AbstractDependent}
+    nodes::Dict{Symbol,Any}
     samplers::Vector{Sampler}
     states::Vector{ModelState}
     iter::Int
