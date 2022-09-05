db319fe6084ae6f5a0c26aebe8c809bdd03e798e
erathorn <wahle.joh@gmail.com>
2022-09-05 11:31:25 +0200

    properly delete

--- src/samplers/sampler.jl
+++ src/samplers/sampler.jl
@@ -8,15 +8,6 @@ const chunksize = 40
 
 struct NullFunction end
 
-# struct SamplingBlock
-#     model::Model
-#     index::Int
-#     transform::Bool
-
-#     SamplingBlock(model::Model, index::Integer = 0, transform::Bool = false) =
-#         new(model, index, transform)
-# end
-
 
 Sampler(param::Symbol, args...) = Sampler([param], args...)
 """
@@ -54,10 +45,8 @@ end
 
 function Base.show(io::IO, s::Sampler)
     print(io, "An object of type \"$(summary(s))\"\n")
-    print(io, "Sampling Block Nodes:\n")
+    print(io, "Sampling Nodes:\n")
     show(io, s.params)
-    #print(io, "\n\n")
-    #show(io, "text/plain", first(code_typed(s.eval)))
     println(io)
 end
 
@@ -87,52 +76,6 @@ end
 
 #################### Simulation Methods ####################
 
-# function gradlogpdf!(
-#     block::SamplingBlock,
-#     x::AbstractArray{T},
-#     dtype::Symbol = :forward,
-# ) where {T<:Real}
-#     gradlogpdf!(block.model, x, block.index, block.transform, dtype = dtype)
-# end
-
-
-# function gradlogpdf!(block::SamplingBlock, x::N) where {N<:GeneralNode}
-#     gradlogpdf!(block.model, x, block.index, block.transform)
-# end
-
-# function logpdf!(block::SamplingBlock, x::AbstractArray{T}) where {T<:Real}
-#     logpdf!(block.model, x, block.index, block.transform)
-# end
-
-# function pseudologpdf!(
-#     block::SamplingBlock,
-#     x::AbstractArray{T},
-#     y::AbstractArray,
-# ) where {T<:Real}
-#     pseudologpdf!(block.model, x, y, block.index, block.transform)
-# end
-
-# function conditional_likelihood!(
-#     block::SamplingBlock,
-#     x::AbstractArray{T},
-#     args...,
-# ) where {T<:Real}
-#     conditional_likelihood!(block.model, x, block.index, args...)
-# end
-
-# function logpdf!(block::SamplingBlock, x::AbstractArray{T}) where {T<:GeneralNode}
-#     logpdf!(block.model, x[1])
-# end
-
-# function logpdf!(block::SamplingBlock, x::T) where {T<:GeneralNode}
-#     logpdf!(block.model, x, block.index, block.transform)
-# end
-
-# function rand!(block::SamplingBlock, x::Int64)
-#     rand!(block.model, x, block.index)
-# end
-
-
 
 function _gradlogpdf!(m::Model, x::AbstractArray, block::Integer, dtype::Symbol = :provided)
     targets = keys(m, :target, block)
@@ -142,17 +85,6 @@ function _gradlogpdf!(m::Model, x::AbstractArray, block::Integer, dtype::Symbol 
     return gradlogpdf(node)
 
 end
-
-# function logpdfgrad!(
-#     block::SamplingBlock,
-#     x::AbstractVector{T},
-#     dtype::Symbol,
-# ) where {T<:Real}
-#     grad = gradlogpdf!(block, x, dtype)
-#     logf = logpdf!(block, x)
-#     (logf, ifelse.(isfinite.(grad), grad, 0.0))
-# end
-
 
 function logpdfgrad!(
     m::Model,
@@ -216,21 +148,6 @@ function logpdf!(
     end
     lp
 end
-
-
-
-
-
-#################### unlist and relist functionality ####################
-
-# function unlist(block::SamplingBlock)
-#     unlist(block.model, block.index, block.transform)
-# end
-
-# function relist(block::SamplingBlock, x::AbstractVariate) where {T<:Real}
-#     relist(block.model, x.value, block.index, block.transform)
-# end
-
 
 #################### Auxiliary Functions ####################
 
